811de7bf93ae32aefcb206b4c8f066392f547e01
MarsWasNotAvailable <marcpayet@laposte.net>
2022-12-07 15:03:05 +0400

    visual hints and cleanup
    display some messages on the document to follow what is happening.

--- 2 Newsletter/subs.php
+++ 2 Newsletter/subs.php
@@ -1,8 +1,4 @@
 <?php
-
-//the table I worked with looked like that:
-//CREATE TABLE IF NOT EXISTS `newsletter`.`subscribed` (`Mail` TEXT NOT NULL , `Date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ) ENGINE = InnoDB; 
-
 	$HostNameOrIP = "localhost";
 	$MySQLUserName = "root";
 	$MySQLPassWord = "";
@@ -16,11 +12,6 @@
 			exit();
 		}
 
-		//TODO: can you react accordingly to below todoes, using connect_errno above
-		//>https://dev.mysql.com/doc/mysql-errors/8.0/en/server-error-reference.html
-		//TODO: or we could use CREATE TABLE IF NOT EXISTS
-		//>this seems simpler
-
 		//TODO: DataBase name shouldnt be hardcoded, there's a variable $DataBaseName above
 		$CreateStatement = $MySQL->prepare("CREATE TABLE IF NOT EXISTS newsletter.subscribed (Mail TEXT NOT NULL , Date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ) ENGINE = InnoDB;");
 		//$CreateStatement->bind_param('s', $DataBaseName);
@@ -30,14 +21,16 @@
 		$UserMail = $_POST["Mail"];
 		$FormIntent = $_POST["FormIntent"];
 		
-		echo $FormIntent;
-		
+		//echo $FormIntent;
 
 		if ($FormIntent == "Unregister")
 		{
 			//Normally, one should notify the mail address and generate some unique link to remove mail from database
 			//Because as it is currently, anybody could script something to send any e-mail address to this server
 			//and it would result in destruction of the list.
+			//there's a builtin mail() in php, it requires some config changes in php.ini (SMTP link)
+			//one would need a Simple Mail Transfer Protocol (SMTP) server: with a domain name like smtp.gmail.com
+			//one can find smtp address in Mail Client settings: smtp.laposte.net, smtp.gmail.com, stmp-mail.outlook.com
 			
 			$DeleteStatement = $MySQL->prepare("DELETE FROM subscribed WHERE Mail = ?");
 			$DeleteStatement->bind_param('s', $UserMail);
@@ -49,6 +42,8 @@
 		}
 		else if ($FormIntent == "Register")
 		{
+			//we're checking email: we dont care about casing, they should all be lowercase
+			//regardless of their input format, the select statement below will match regardless of case.
 			$MySQLStatement = $MySQL->prepare("SELECT * FROM subscribed WHERE Mail= ?");
 			//first argument is type: i(nt), d(ouble), s(tring), b(lob) -> 'sss' for three strings
 			$MySQLStatement->bind_param('s', $UserMail);
@@ -56,7 +51,8 @@
 			
 			if ( ($MySQLStatement->get_result()->num_rows) < 1 )
 			{
-				//there's a REPLACE command one could use as a insert unique only, but some claims it's slower than SELECT + UPDATE
+				//there's a REPLACE command one could use as a insert unique only,
+				//but some claims it's slower than SELECT + UPDATE
 				//>it's actually deleting the record if it exists already
 				
 				$MySQLStatement->prepare("INSERT INTO subscribed (Mail, Date) VALUES (?, current_timestamp())");
